24f95b4c535113d547c14573a8cbffa386344bfe
Firsoff Maxim <marchosf@googlemail.com>
2020-12-24 22:19:15 +0300

    trying to wirite scoretable in file

--- lab3/rounds.py
+++ lab3/rounds.py
@@ -1,83 +1,102 @@
 import pygame
 from pygame.draw import *
 from random import randint
+
 pygame.init()
 
 FPS = 2
-ballsnamber = 5 # namber of balls
+ballsnumber = 5  # number of balls
 
 # get screen dimensions
 screenInfo = pygame.display.Info()
 
-# displey's coordinate
+# display's coordinate
 xcoord = screenInfo.current_w - 400
 ycoord = screenInfo.current_h - 100
 
-py = [1]*ballsnamber # delta for x moving
-px = [1]*ballsnamber # delta for y moving
+py = [1] * ballsnumber  # delta for x moving
+px = [1] * ballsnumber  # delta for y moving
 
 # angel of fly`s ball
-for n in range (ballsnamber):
+for n in range(ballsnumber):
     px[n] = randint(-10, 10)
     py[n] = randint(-10, 10)
 
 screen = pygame.display.set_mode((xcoord, ycoord))
 
-score = 0 # Score
+score = 0  # Score
 
 COLORS = [(255, 0, 0), (0, 0, 255), (255, 255, 0), (0, 255, 0), (255, 0, 255), (0, 255, 255)]
 
-r = [0]*ballsnamber # radius for each ball
-x = [0]*ballsnamber # x for each ball
-y = [0]*ballsnamber # y for each ball
-color = [0]*ballsnamber # color for each ball
+r = [0] * ballsnumber  # radius for each ball
+x = [0] * ballsnumber  # x for each ball
+y = [0] * ballsnumber  # y for each ball
+color = [0] * ballsnumber  # color for each ball
 
 
 def new_ball(cx):
-    '''рисует новый шарик '''
+    """рисует новый шарик """
     global x, y, r, color
     r[cx] = randint(5, 50)
-    x[cx] = randint(r[cx], xcoord-r[cx])
-    y[cx] = randint(r[cx], ycoord-r[cx])
+    x[cx] = randint(r[cx], xcoord - r[cx])
+    y[cx] = randint(r[cx], ycoord - r[cx])
     color[cx] = COLORS[randint(0, 5)]
     circle(screen, color[cx], (x[cx], y[cx]), r[cx])
 
+
 def click(event):
     global score
-    print(x, y, r)
-    for cx in range(ballsnamber):
+    for cx in range(ballsnumber):
         if abs(x[cx] - event.pos[0]) < r[cx] and abs(y[cx] - event.pos[1]) < r[cx]:
-            print("Ooopsssss")
-            score += int(1/r[cx]*100*(x[cx]+y[cx]))
+
+            score += int(1 / r[cx] * 100 * (x[cx] + y[cx]))
             new_ball(cx)
+        else:
+            score -= 410
+
+
+
+def filescore(score):
+    fileline = []
+    with open('ballscore.txt') as file:
+        for line in file:
+            fileline.append(line.split(" "))
+        fileline.append([score, 'YOU,\n'])
+        fileline.sort(key=lambda x: int(x[0]))
+
+    filescore = open('ballscore.txt', "w")
+    print(fileline, file = filescore)
+    filescore.close()
+
 
 clock = pygame.time.Clock()
 finished = False
 
 # generating of balls
-for cx in range(ballsnamber):
+for cx in range(ballsnumber):
     new_ball(cx)
 
 while not finished:
     clock.tick(FPS + 30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            filescore(score)
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             click(event)
 
-    for cx in range(ballsnamber):
+    for cx in range(ballsnumber):
         circle(screen, color[cx], (x[cx], y[cx]), r[cx])
         x[cx] += px[cx]
         y[cx] += py[cx]
-        if abs(x[cx]) - r[cx] < 0 or x[cx] + r[cx] > xcoord: # if our ball is near the wall
+        if abs(x[cx]) - r[cx] < 0 or x[cx] + r[cx] > xcoord:  # if our ball is near the wall
             px[cx] *= -1
-        if abs(y[cx]) - r[cx] < 0 or y[cx] + r[cx] > ycoord: # if our ball is near the wall
+        if abs(y[cx]) - r[cx] < 0 or y[cx] + r[cx] > ycoord:  # if our ball is near the wall
             py[cx] *= -1
 
-    # for Score displaied
+    # for Score displayed
     f1 = pygame.font.Font(None, 36)
-    text1 = f1.render("Score: " + str(score), 0, (180, 120, 120))
+    text1 = f1.render("Score: " + str(score), True, (180, 120, 120))
     screen.blit(text1, (10, 50))
 
     pygame.display.update()
