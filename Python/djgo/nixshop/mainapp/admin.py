092aa718f39d15e7b8d02e6b7df5a115b9ed2ae5
Firsoff Maxim <marchosf@googlemail.com>
2021-02-12 20:40:47 +0300

    added market.yandex iteams, correct ozon for iteams without discount

--- Python/djgo/nixshop/mainapp/admin.py
+++ Python/djgo/nixshop/mainapp/admin.py
@@ -1,3 +1,32 @@
+from django import forms
+from django.forms import ModelChoiceField
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+
+class NotebookAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "category":
+            return ModelChoiceField(Category.objects.filter(slug="notebooks"))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphonesAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "category":
+            return ModelChoiceField(Category.objects.filter(slug="smartphones"))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphonesAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
+
